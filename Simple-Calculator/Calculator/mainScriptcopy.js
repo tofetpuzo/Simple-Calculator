6de0fbc9d17ba7943b1668ca524ba482bdc69133
tofetpuzo <topetemiola09@gmail.com>
2022-02-28 18:04:08 +0000

    Simple Calculator

---
+++ Simple-Calculator/Calculator/mainScriptcopy.js
@@ -0,0 +1,50 @@
+var flag = false;
+var temp = '';
+const operators = ["+", "-", "*", "/"];
+
+
+function insert(data) {
+    //for (data in operators) {
+    for (i = 0; i < operators.length; i++) {
+        if (data == operators[i]) {
+            temp = document.getElementById('res').value + data;
+            clears();
+            //backspace();
+            flag = true
+        }
+
+    }
+
+    if (flag) {
+        document.getElementById('res').value = document.getElementById('res').value + data;
+        flag = false;
+    }
+    document.getElementById('res').value = document.getElementById('res').value + data;
+}
+
+
+function backspace() {
+    var cur_val = document.getElementById('res').value
+    document.getElementById('res').value = cur_val.substring(0, cur_val.length - 1);
+}
+
+
+function clears() {
+    s = document.getElementById('res').value = '';
+    return s
+}
+
+
+function operation() {
+    var exp = document.getElementById('res').value
+    if (exp) {
+        if (temp) {
+            document.getElementById('res').value = eval(temp + exp);
+
+        } else {
+            document.getElementById('res').value = eval(exp);
+        }
+        flag = true;
+    }
+}
+
